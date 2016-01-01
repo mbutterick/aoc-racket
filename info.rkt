09883ce619ad02042c78c2cf1f9889cc83aefb70
Matthew Butterick <mb@mbtype.com>
2016-01-01 07:17:23 -0800

    rename

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "aoc-racket")
-(define scribblings '(("start.scrbl" (multi-page))))
+(define scribblings '(("main.scrbl" (multi-page))))
 (define deps '("base" "scribble-lib"))
 (define build-deps '("rackunit-lib" "racket-doc" "scribble-doc" "rackunit-doc" "at-exp-lib"))
