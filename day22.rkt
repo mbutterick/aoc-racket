a9c41325d2c56af93100d02af4f6d73e4947ea46
Matthew Butterick <mb@mbtype.com>
2016-01-05 23:03:46 -0800

    fix day22

--- day22.rkt
+++ day22.rkt
@@ -6,11 +6,13 @@
 
 @link["http://adventofcode.com/day/22"]{The puzzle}. Our @link-rp["day22-input.txt"]{input} tells us our hit points and damage.
 
+@chunk[<day22>
+       ;;
+       ]       
 
+@section{What's the least mana we can spend and win?}
 
-@section{What's the least we can spend and win?}
-
-Did I mention I hate RPGs? Well, this question is much more RPG-ish than @secref{Day_21}. It involves spellscasting. So I'm taking a pass. If someone devises a concise solution, I'll be happy to add it to this page.
+Did I mention I hate RPGs? Well, this question is much more RPG-ish than @secref{Day_21}. It involves mana and spellscasting. So I'm taking a pass. If someone devises a concise solution, I'll be happy to add it to this page.
 
 Otherwise, onward to @secref{Day_23}.
 
