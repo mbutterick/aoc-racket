bc5f33df4d6dfb5b32bce814e4fd672097661522
Matthew Butterick <mb@mbtype.com>
2016-12-01 00:13:19 -0800

    day01,p1

---
+++ 2016/day01.rkt
@@ -0,0 +1,33 @@
+#lang br/quicklang
+(require racket/file)
+(module+ reader
+  (provide read-syntax)
+  (define (read-syntax path port)
+    (strip-bindings
+     #`(module day01-mod "day01.rkt"
+         #,@(for/list ([t (in-list (string-split (port->string port) ", "))])
+                      `(turn ,@(string-split (string-trim t) #px"(?<=[LR])")))))))
+
+(define-macro (mb . TURNS)
+  #'(#%module-begin
+     (solve . TURNS)))
+(provide (rename-out [mb #%module-begin]))
+
+(define (solve . turns)
+  (define loc
+       (let loop ([loc 0]
+                  [dir 1]
+                  [turns turns])
+         (if (empty? turns)
+             loc
+             (let* ([turn (car turns)]
+                    [rotation (car turn)]
+                    [dist (cdr turn)]
+                    [new-dir (* dir rotation)])
+               (loop (+ loc (* new-dir dist)) new-dir (cdr turns))))))
+     (+ (abs (imag-part loc)) (abs (real-part loc))))
+
+(define-macro-cases turn
+  [(turn "L" DIST) #'(cons +i (string->number DIST))]
+  [(turn "R" DIST) #'(cons -i (string->number DIST))])
+(provide turn)
--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "aoc-racket")
 (define scribblings '(("aoc-racket.scrbl" (multi-page))))
-(define deps '("base" "scribble-lib" "sugar" "rackunit-lib" "math-lib"))
+(define deps '("base" "scribble-lib" "sugar" "rackunit-lib" "math-lib" "beautiful-racket-lib"))
 (define test-omit-paths (list #rx"rkt$"))
 (define build-deps '("rackunit-lib" "racket-doc" "scribble-doc" "rackunit-doc" "at-exp-lib" "math-doc"))
