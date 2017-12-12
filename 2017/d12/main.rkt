2b0fdd766bd08b7ee21ba1f2a21d644d8c742efb
Matthew Butterick <mb@mbtype.com>
2017-12-11 22:58:35 -0800

    d12

---
+++ 2017/d12/main.rkt
@@ -0,0 +1,28 @@
+#lang reader "../aoc-lang.rkt"
+(require graph)
+(provide (rename-out [#%mb #%module-begin]))
+
+(define-macro (#%mb (STARS) (NUM <-> . NUMS) ...)
+  #'(#%module-begin
+     (define g (unweighted-graph/undirected null))
+     (for-each (curry add-edge! g NUM) (list . NUMS)) ...     
+     (if (eq? 'STARS '★)
+         (programs-in-group g 0)
+         (number-of-groups g (list NUM ...)))))
+
+(define (programs-in-group g x) (length (group-of g x)))
+
+(define (group-of g x)
+  (define-values (connects _) (dijkstra g x))
+  (for/list ([(k v) (in-hash connects)]
+             #:when (integer? v))
+    k))
+
+(define (number-of-groups g nums)
+  (for/fold ([nums-seen null]
+             [group-count 0]
+             #:result group-count)
+            ([num (in-list nums)]
+             #:unless (memv num nums-seen))
+    (values (append (group-of g num) nums-seen) (add1 group-count))))
+
