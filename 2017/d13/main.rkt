379cd904b507e99f28dd8823c373385d811d2152
Matthew Butterick <mb@mbtype.com>
2017-12-12 22:24:07 -0800

    d13

---
+++ 2017/d13/main.rkt
@@ -0,0 +1,25 @@
+#lang reader "../aoc-lang.rkt"
+(require (for-syntax racket/string racket/sequence) racket/dict)
+(provide (rename-out [#%mb #%module-begin]) ★ ★★)
+
+(define-macro (#%mb (STARS) (DEPTH: RANGE) ...)
+  (with-pattern ([(DEPTH ...)
+                  (for/list ([id (in-syntax #'(DEPTH: ...))])
+                    (string->number (string-trim (symbol->string (syntax->datum id)) ":")))])
+    #'(#%module-begin (STARS '(DEPTH ...) '(RANGE ...)))))
+
+(define (caught? depth range [delay 0])
+  (zero? (modulo (+ depth delay) (* 2 (sub1 range)))))
+
+(define (★ ds rs)
+  (for/sum ([d (in-list ds)]
+            [r (in-list rs)]
+            #:when (caught? d r))
+    (* d r)))
+
+(define (★★ ds rs)
+  (for/first ([delay (in-naturals)]
+              #:unless (for/or ([d (in-list ds)]
+                                [r (in-list rs)])
+                         (caught? d r delay)))
+    delay))
---
+++ 2017/d13/test2.rkt
@@ -0,0 +1,5 @@
+#lang reader "main.rkt" ★★ 
+0: 3
+1: 2
+4: 4
+6: 4
