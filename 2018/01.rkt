5718ef6aad50a971f0e9b45b5a41987a306d7848
Matthew Butterick <mb@mbtype.com>
2018-12-01 09:22:51 -0800

    d1

---
+++ 2018/01.rkt
@@ -0,0 +1,23 @@
+#lang debug br
+(require racket/set)
+
+(define fp (open-input-file "01.txt"))
+
+(define frequencies
+  (map string->number (port->lines fp)))
+
+(define (*)
+  (apply + frequencies))
+
+(define (**)
+  (for/fold ([last-sum 0]
+             [sums (set)]
+             #:result last-sum)
+            ([freq (in-cycle frequencies)]
+             #:break (set-member? sums last-sum))
+    (values (+ freq last-sum) (set-add sums last-sum))))
+
+(module+ main
+  (require rackunit)
+  (check-equal? (time (*)) 454)
+  (check-equal? (time (**)) 566))
