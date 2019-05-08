04f42d786c0c207521a35f5405fb808e2f378e6a
Matthew Butterick <mb@mbtype.com>
2019-05-08 13:42:33 -0700

    gregorize

--- info.rkt
+++ info.rkt
@@ -2,6 +2,17 @@
 (define collection "aoc-racket")
 (define scribblings '(("aoc-racket.scrbl" (multi-page))))
 (define deps '("graph"
-               "base" "scribble-lib" ("sugar" #:version "0.3") "rackunit-lib" "math-lib" "beautiful-racket-lib"))
+               "base"
+               "scribble-lib"
+               ("sugar" #:version "0.3")
+               "rackunit-lib"
+               "math-lib"
+               "beautiful-racket-lib"
+               "gregor"))
 (define test-omit-paths (list #rx"rkt$"))
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-doc" "rackunit-doc" "at-exp-lib" "math-doc"))
+(define build-deps '("rackunit-lib"
+                     "racket-doc"
+                     "scribble-doc"
+                     "rackunit-doc"
+                     "at-exp-lib"
+                     "math-doc"))
